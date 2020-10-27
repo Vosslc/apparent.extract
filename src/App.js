3f1c80df1d06ed88463fbd0855fc17e709ce875a
Roundy <nroundy801@gmail.com>
2020-10-27 11:20:18 -0600

    homepage had been organized into smaller components, I have also finished styling for the menue items

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import Homepage from '../src/Components/Homepage'
+import Homepage from '../src/pages/hompage/homepage.component'
 
 function App() {
   return (
