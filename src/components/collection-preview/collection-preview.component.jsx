2ac12665e5109662016a85420df863514701591f
Roundy <nroundy801@gmail.com>
2021-01-07 15:17:14 -0700

    resolved issue with .map and spread. Tied in scss styling.

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import CollectionItem from "../collection-item/collection-item.component";
 
-
 import "./collection-preview.styles.scss";
 
 const CollectionPreview = ({ title, items }) => (
@@ -10,7 +9,7 @@ const CollectionPreview = ({ title, items }) => (
     <div className='preview'>
       {items
         .filter((item, inx) => inx < 4)
-        .map((id, ...otherItemProps) => (
+        .map(({ id, ...otherItemProps }) => (
           <CollectionItem key={id} {...otherItemProps} />
         ))}
     </div>
